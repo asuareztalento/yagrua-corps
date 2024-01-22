c88a84fd84cbb51be4cd9998525a9180f3ed6884
Alberto Suárez <asuarez@talentosoftware.com>
2024-01-22 09:55:15 +0100

    user 'onDuty' field

--- src/resources/users/dto/update-user.dto.ts
+++ src/resources/users/dto/update-user.dto.ts
@@ -1,4 +1,10 @@
 import { OmitType, PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
+import { OnDutyStatuses } from '../entities/user.entity';
+import { IsEnum, IsString } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(OmitType(CreateUserDto, ['auth'] as const)) {}
+export class UpdateUserDto extends PartialType(OmitType(CreateUserDto, ['auth'] as const)) {
+  @IsEnum(OnDutyStatuses)
+  @IsString()
+  onDuty: OnDutyStatuses;
+}
--- src/resources/users/entities/user.entity.ts
+++ src/resources/users/entities/user.entity.ts
@@ -1,6 +1,15 @@
 import { BasicEntity } from 'src/utils/basic.entity';
 import { Column, Entity } from 'typeorm';
 
+export enum OnDutyStatuses {
+  // user is ready to attend a new service
+  AVAILABLE = 'AVAILABLE',
+  // the user set at the app that is not available
+  ABSENT = 'ABSENT',
+  // The user is currently at a service
+  ON_SERVICE = 'ON_SERVICE',
+}
+
 @Entity('users')
 export class User extends BasicEntity {
   @Column()
@@ -23,6 +32,13 @@ export class User extends BasicEntity {
 
   @Column({ default: false, nullable: true })
   occupationalRiskPreventionTraining: boolean;
+
+  @Column({
+    default: OnDutyStatuses.AVAILABLE,
+    enum: OnDutyStatuses,
+    type: 'enum',
+  })
+  onDuty: OnDutyStatuses;
 
   @Column({ default: '', nullable: true })
   otherTraining: string;
