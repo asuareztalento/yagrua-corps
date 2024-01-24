c4ce5a8708fdfd15ba7c89580f820dfa47a2bbbb
Alberto Suárez <asuarez@talentosoftware.com>
2024-01-24 19:15:02 +0100

    FIX optional 'onDuty' user field at dto

--- src/resources/users/dto/update-user.dto.ts
+++ src/resources/users/dto/update-user.dto.ts
@@ -1,10 +1,11 @@
 import { OmitType, PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
 import { OnDutyStatuses } from '../entities/user.entity';
-import { IsEnum, IsString } from 'class-validator';
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 
 export class UpdateUserDto extends PartialType(OmitType(CreateUserDto, ['auth'] as const)) {
   @IsEnum(OnDutyStatuses)
   @IsString()
-  onDuty: OnDutyStatuses;
+  @IsOptional()
+  onDuty?: OnDutyStatuses;
 }
